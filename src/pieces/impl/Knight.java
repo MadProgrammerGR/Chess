e83765ac582db5765a01fbf71f3264bb5a16b0bf
JRyuzaki <zivanovicjovanprivatemail@gmail.com>
2016-09-04 17:25:53 +0200

    Add dummy Piece-Implementations

---
+++ src/pieces/impl/Knight.java
@@ -0,0 +1,29 @@
+package pieces.impl;
+
+import java.util.List;
+
+import pieces.AbstractPiece;
+import pieces.PieceType;
+import pieces.Player;
+import util.Position;
+
+public class Knight extends AbstractPiece{
+
+	public Knight(PieceType type, Player player) {
+		super(PieceType.KNIGHT, player);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isMoveValid(Position oldPosition, Position newPosition) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Position> getValidMoces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/pieces/impl/Rook.java
@@ -0,0 +1,29 @@
+package pieces.impl;
+
+import java.util.List;
+
+import pieces.AbstractPiece;
+import pieces.PieceType;
+import pieces.Player;
+import util.Position;
+
+public class Rook extends AbstractPiece{
+
+	public Rook(PieceType type, Player player) {
+		super(PieceType.ROOK, player);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isMoveValid(Position oldPosition, Position newPosition) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Position> getValidMoces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
